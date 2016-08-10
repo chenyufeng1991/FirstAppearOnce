89aad505a6b5141edf2e68117481fe658d988ea7
陈宇峰 <yufengcode@gmail.com>
2016-08-10 13:35:22 +0800

    基本实现功能。

--- FirstAppearOnce/main.cpp
+++ FirstAppearOnce/main.cpp
@@ -8,8 +8,52 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+/**
+ *  一个字符就是一个字节，一个字节为8位，因此创建一个长为256的数组。以字符ASCII码为键值。
+ *
+ *  char：一个字符是一个字节，占8位。
+ 
+ ASCII码占用一个字节，可以有0~255共256个取值。前128个为常用的字符如运算符，字母，数字等，是可以在键盘上显示的。
+ 
+后128个位特殊字符是键盘上找不到的字符。
+ 
+ */
+#define CHAR_SIZE 256
+
+int main(int argc, const char * argv[])
+{
+    int hashTable[CHAR_SIZE];
+    for (int i = 0; i < CHAR_SIZE; i++)
+    {
+        hashTable[i] = 0;
+    }
+
+    string myString = "abcdab";
+    for (int i = 0 ; i < myString.length(); i++)
+    {
+        hashTable[myString[i]]++;
+    }
+
+    for (int i = 0; i < myString.length(); i++)
+    {
+        if (hashTable[myString[i]] == 1)
+        {
+            cout << "第一次只出现一次的字符：" << myString[i] << endl;
+            return 0;
+        }
+    }
+    cout << "不存在这样的字符" << endl;
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
